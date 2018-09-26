71ab661c3d564e9c3e979d905f3c8cb9fdc6424d
Matthias Hosp <matthiashosp@Matthiass-MacBook-Pro.local>
2018-09-26 14:15:40 +0200

    hi

---
+++ Assets/scripts/NumberWizard.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberWizard : MonoBehaviour {
+
+    int min = 1;
+    int max = 1000;
+    int guess;
+
+    void setGuess()
+    {
+        guess = ((min + max) / 2);
+        Debug.Log("guess " + guess);
+    }
+
+    // Use this for initialization
+    void Start () {
+
+        setGuess();
+       
+        // kleiner
+        if (false)
+        {
+            max = guess;
+            Debug.Log("max " + max);
+        }
+
+        // groeßer
+        if (true)
+        {
+            min = guess;
+            Debug.Log("min " + min);
+        }
+
+        //gleich
+        if (false){
+        Debug.Log("gefunden ");
+        }
+
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
