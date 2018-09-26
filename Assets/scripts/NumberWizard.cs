6b71dce5a8a9c70abe9fd1d30778c47ef3171c33
Matthias Hosp <matthiashosp@Matthiass-MacBook-Pro.local>
2018-09-26 14:51:16 +0200

    1st prototype

--- Assets/scripts/NumberWizard.cs
+++ Assets/scripts/NumberWizard.cs
@@ -8,41 +8,51 @@ public class NumberWizard : MonoBehaviour {
     int max = 1000;
     int guess;
 
-    void setGuess()
-    {
-        guess = ((min + max) / 2);
-        Debug.Log("guess " + guess);
-    }
+
 
     // Use this for initialization
     void Start () {
 
         setGuess();
        
+    }
+
+    void setGuess()
+    {
+        guess = ((min + max) / 2);
+        Debug.Log(guess + "? ist das d1 Number mois");
+        //Start ();
+    }
+
+    void askPlayer()
+    {
+
         // kleiner
-        if (false)
+        if (Input.GetKeyDown(KeyCode.DownArrow))
         {
             max = guess;
-            Debug.Log("max " + max);
+            //  Debug.Log("max " + max);
+            setGuess();
         }
 
         // groeßer
-        if (true)
+        if (Input.GetKeyDown(KeyCode.UpArrow))
         {
             min = guess;
-            Debug.Log("min " + min);
+            // Debug.Log("min " + min);
+            setGuess();
         }
 
         //gleich
-        if (false){
-        Debug.Log("gefunden ");
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            Debug.Log("super fresh, i hans");
         }
 
-
     }
-	
-	// Update is called once per frame
-	void Update () {
-		
+    // Update is called once per frame
+    void Update () {
+     //   Debug.Log("Test");
+        askPlayer();
 	}
 }
